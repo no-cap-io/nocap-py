9103595c51ac1c6d2529e2bea7934b4496d180fa
Lick <73350679+Lick@users.noreply.github.com>
2022-03-06 13:15:23 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,8 @@
 from nocap import NoCap
 
 solver = NoCap(api_key="API_KEY")
-solution = solver.hcaptcha(mode=3, host="host", sitekey="sitekey", proxy="http://user:pass@ip:port")
-print(solution)
+try:
+    solution = solver.hcaptcha(mode=3, host="host", sitekey="sitekey", proxy="http://user:pass@ip:port")
+    print(solution)
+except Exception as e:
+    print(e)
